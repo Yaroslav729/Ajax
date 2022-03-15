29e8bd666dc36a2ca1b2e0052419b9de825e2698
Yroslav Shishkin <company-rosstan@yandex.ru>
2022-03-15 10:33:59 +0300

    lesson26

--- script.js
+++ script.js
@@ -7,15 +7,16 @@ const getData = async (url) => {
     return await user.json()
 
 }
-getData('db.json').then((data))
 
 const sendData = async (url, data) => {
     const user = await fetch(url,{
-        metod: "Post",
-        body: data,
+        method: "Post",
+        body: JSON.stringify(data)
     })
     if (!user.ok) {
         throw new Error(`Ошибка`)
     }
     return await user.json()
+
 }
+getData('db.json').then((data) => sendData("https://jsonplaceholder.typicode.com/posts", data))
